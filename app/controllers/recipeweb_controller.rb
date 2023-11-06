d16e0801dab9234ba3c9740e3ceed8f2f394712d
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-11-06 19:34:08 +0300

    Fix routes and finish the devise authentication views

--- app/controllers/recipeweb_controller.rb
+++
@@ -1,3 +0,0 @@
-class RecipewebController < ApplicationController
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'recipeweb#index'
+  get '/sign_out_user', to: 'users#sign_out_user', as: 'sign_out_user'
+  devise_for :users
+  root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
