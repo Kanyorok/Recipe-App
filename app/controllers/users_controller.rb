6c8b576ddefa07cd89cc299baf54c8245e2afff0
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-11-08 12:58:03 +0300

    Fix the linter errors in the project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
-	before_action :authenticate_user!
-	def index
+  before_action :authenticate_user!
+  def index
     @users = User.all
     @user = current_user
   end
 
-	def sign_out_user
+  def sign_out_user
     sign_out(current_user)
     redirect_to root_path, notice: 'Signed out successfully'
   end
