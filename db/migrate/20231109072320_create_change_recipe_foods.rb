2bc5d09091a2fb4b8562404a2a7bf5931b58164b
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-11-09 19:58:31 +0300

    Add the public view for the public recipes

--- db/migrate/20231109072320_create_change_recipe_foods.rb
+++
@@ -1,6 +0,0 @@
-class AddcolumnRecipeFoods < ActiveRecord::Migration[7.0]
-  def change
-    add_column :recipe_foods, :food_name, :string
-    add_column :recipe_foods, :recipe_name, :string
-  end
-end
